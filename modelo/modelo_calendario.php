5946933cf62ce4b1b919d467194d21d31a8dfe4b
Andres Herrera <afherrera028@misena.edu.co>
2020-12-02 16:31:09 -0500

    se crea archivo modelo_calendario.php en la carpeta modelo con el respectivo codigo

---
+++ modelo/modelo_calendario.php
@@ -0,0 +1,170 @@
+<html>
+<head>
+<title>calendario</title>
+</head>
+<body>
+<?php
+
+require_once("modelo_conexion.php");
+
+class calendario extends conexion 
+{
+	private $id_calendario;
+	private $nombre_calendario;
+	private $estado_calendario;
+	private $fecha_calendario;
+	private $descripcion_calendario;
+  private $funcionario;
+  private $conec;
+
+public function __construct()
+{
+      
+       $this->conec = new conexion();
+       $this->conec = $this->conec->conectar();
+}
+
+public function setId($identificador)
+{
+       $this->id_calendario=$identificador;
+}
+public function getId()
+{
+       return $this->id_calendario;
+}
+
+
+public function setNombre($nombre)
+{
+       $this->nombre_calendario=$nombre;	
+}
+public function getNombre()
+{
+       return $this->nombre_calendario;	
+}
+
+
+public function setEstado($estado)
+{
+       $this->estado_calendario=$estado;	
+}
+public function getEstado()
+{
+       return $this->estado_calendario;	
+}
+
+
+public function setFecha($fecha)
+{
+       $this->fecha_calendario=$fecha;	
+}
+public function getFecha()
+{
+       return $this->fecha_calendario;	
+}
+
+public function setDescripcion($descripcion)
+{
+       $this->descripcion_calendario=$descripcion;	
+}
+public function getDescripcion()
+{
+       return $this->descripcion_calendario;	
+}
+public function setFuncionario($funcionario)
+{
+        $this->funcionario = $funcionario;  
+}
+public function getFuncionario()
+{
+       return $this->funcionario;  
+}
+public function insertar($tabla,$datos)
+{
+ $sql="insert into ".$tabla." 
+ (id_calendario,nombre,estado,fecha,descripcion,funcionario) values
+ ('".$this->getId()."',
+ '".$this->getNombre()."',
+ '".$this->getEstado()."',
+ '".$this->getFecha()."',
+ '".$this->getDescripcion()."',
+ '".$this->getFuncionario()."'    
+    )";
+ $resultado = $this->conec->query($sql);
+ if ($resultado) 
+ {
+ 	return true;
+ }
+ else
+ {
+ 	return false;
+ }
+}
+
+public function contar()
+   {
+       $sql="select count(*) as num_cal from calendario";
+       $conteo=$this->conec->query($sql);
+       if($conteo)
+       {
+           return $conteo->fetch_all(MYSQLI_ASSOC);; 
+       }
+       else
+       {
+           return false;
+       }
+   }
+
+public function buscar($tabla,$condicion)
+   {
+      if ($condicion==null)
+      {
+         $sql="Select * from ".$tabla;
+      }
+      else
+      {
+         $sql="Select * from ".$tabla." where id_calendario = ".$condicion; 
+      }
+       
+      $resultado=$this->conec->query($sql);
+      if($resultado)
+      {
+          return $resultado->fetch_all(MYSQLI_ASSOC);
+      }
+      else
+      {     
+          return false;
+      } 
+   }
+   
+public function actualizar($tabla,$datos)
+{
+       $sql="update ".$tabla." set nombre = '".$this->getNombre()."', estado = '".$this->getEstado()."', fecha = '".$this->getFecha()."', descripcion = '".$this->getDescripcion()."' WHERE id_calendario = '".$this->getId()."'";
+       $resultado = $this->conec->query($sql);
+       if ($resultado)
+       {
+           return true;
+       }
+       else 
+       {
+           return false;
+       }
+}
+
+public function borrar($tabla,$condicion)
+{
+ $sql="delete from ".$tabla." where id_calendario =".$condicion;
+      $resultado=$this->conec->query($sql);
+      if ($resultado)
+       {
+           return true;
+       }
+       else 
+       {
+           return false;
+       }   
+   }     
+   
+}
+
+?>
--- vista/modulos/calendario/vista_crear_calendario.php
+++ vista/modulos/calendario/vista_crear_calendario.php
@@ -1,211 +1,93 @@
 <?php
 //Importamos los archivos de funciones y la clase a manipular de la capa modelo.
-require_once("../../controlador/Controlador_Funciones.php");
-require_once("../../modelo/modelo_categoria.php");
-
-
+require_once("../../../controlador/Controlador_Funciones.php");
+require_once("../../../modelo/modelo_calendario.php");
 //Guarda los valores de los campos en variables, siempre y cuando se haya enviado del formulario, sino se guardará null.
-$nombre = isset($_POST['nombre']) ? $_POST['nombre'] : null;
-$descripcion = isset($_POST['descripcion']) ? $_POST['descripcion'] : null;
-
+$id_calendario = isset($_POST['id_calendario']) ? $_POST['id_calendario'] : null;
+$nombre_calendario = isset($_POST['nombre_calendario']) ? $_POST['nombre_calendario'] : null;
+$estado_calendario = isset($_POST['estado_calendario']) ? $_POST['estado_calendario'] : null;
+$fecha_calendario = isset($_POST['fecha_calendario']) ? $_POST['fecha_calendario'] : null;
+$descripcion_calendario = isset($_POST['descripcion_calendario']) ? $_POST['descripcion_calendario'] : null;
+$funcionario = isset($_POST['funcionario']) ? $_POST['funcionario'] : null;
+//$funcionario = isset($_POST['funcionario']) ? $_POST['funcionario'] : null;
 //Este array guardará los errores de validación que surjan.
 $errores= array();
 //Pregunta si está llegando una petición por POST, lo que significa que el usuario envió el formulario.
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     //Valida que el campo documento usuario, no esté vacío.
-   if (!validaRequerido($nombre)) {
-      $errores[] = 'El nombre de la categoría es requerida, no sea digitado. !INCORRECTO!.';
-   }
    //Valida que el campo clave usuario, no esté vacío.
-   if (!validaRequerido($descripcion))
-   {
-      $errores[] = 'La descripción es requerida, no se a digitado. !INCORRECTO!.';
+   if (!validaRequerido($id_calendario)) {
+      $errores[] = 'El numero del calendario es requerido, no sea digitado. !INCORRECTO!.';
    }
-       
+
+
+   //Valida que el campo clave usuario, no esté vacío.
+   if (!validaRequerido($nombre_calendario))
+   {
+      $errores[] = 'El nombre del calendario es requerido, no se a digitado. !INCORRECTO!.';
+   }
+   //Valida que el email usuario, no esté vacío.
+   if (!validaRequerido($estado_calendario))
+   {
+      $errores[] = 'El estado del calendario es requerido, no se a digitado. !INCORRECTO!.';
+   }
+   if (!validaRequerido($fecha_calendario))
+   {
+      $errores[] = 'La fecha del calendario es requerida, no se a digitado. !INCORRECTO!.';
+   }    
+
+   if (!validaRequerido($descripcion_calendario))
+   {
+      $errores[] = 'La descripcion del usuario es requerida, no se a digitado. !INCORRECTO!.';
+   }    
+   //Valida que el campo documento sea numérico y no esté vacío.        
+   if (!validaNumero($id_calendario))
+   {
+      $errores[] = 'El numero del calendario es de tipo numérico, !INCORRECTO!.';
+   }
+
+   if (!validaRequerido($funcionario))
+   {
+      $errores[] = 'El funcionario es requerido, no se a digitado. !INCORRECTO!.';
+   }
+    
     //Verifica si se han encontrado errores y de no haber, se implementa la lógica del programa.
     
     if(!$errores)
     {
-        //require_once("vista_usuario.php");
-        $cat=new categoria();
-        $cat->setNombre($nombre);
-        $cat->setDescripcion($descripcion);
-        if ($cat->insertar("categoria",null))
+        require_once("vista_calendario.php");
+        $cal=new calendario();
+        $cal->setId($id_calendario);
+        $cal->setNombre($nombre_calendario);
+        $cal->setEstado($estado_calendario);
+        $cal->setFecha($fecha_calendario);     
+        $cal->setDescripcion($descripcion_calendario);
+        $cal->setFuncionario($funcionario);
+
+        if ($cal->insertar("calendario",null))
         {
-          echo "Registro adicionado";  
+          echo header("location: vista_calendario.php");   
         }
         else
         {
-          //echo "Registro, no se puede adicionar, porque ya existe un registro con el documento digitado";
+          echo "Registro, no se puede adicionar, porque ya existe un registro con el numero de calendario digitado";
         }
     }    
 }
 ?>
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>sockedOp | Escuela de Futbol</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <!-- Bootstrap 3.3.5 -->
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/bootstrap-select.min.css">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="../css/font-awesome.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="../css/AdminLTE.min.css">
-    <!-- AdminLTE Skins. Choose a skin from the css/skins
-         folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="../css/_all-skins.min.css">
-    <link rel="apple-touch-icon" href="../img/apple-touch-icon.png">
-    <link rel="shortcut icon" href="../img/favicon.ico">
-
-  </head>
+<html lang="es">
+    <?php
+    require_once("../../partials/head.php");
+  ?>
   <body class="hold-transition skin-blue sidebar-mini">
     <div class="wrapper">
 
-      <header class="main-header">
-
-        <!-- Logo -->
-        <a href="index2.html" class="logo">
-          <!-- mini logo for sidebar mini 50x50 pixels -->
-          <span class="logo-mini"><b><img src="../img/V20.png" height="20px" width="45px"></b></span>
-          <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b><img src="../img/V20.png" height="50px" width="200px"></b></span>
-        </a>
-
-        <!-- Header Navbar: style can be found in header.less -->
-        <nav class="navbar navbar-static-top" role="navigation">
-          <!-- Sidebar toggle button-->
-          <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-            <span class="sr-only">Navegación</span>
-          </a>
-          <!-- Navbar Right Menu -->
-          <div class="navbar-custom-menu">
-            <ul class="nav navbar-nav">
-              <!-- Messages: style can be found in dropdown.less-->
-              
-              <!-- User Account: style can be found in dropdown.less -->
-              <li class="dropdown user user-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <small class="bg-red">Online</small>
-                  <span class="hidden-xs">Patricia Vallejos</span>
-                </a>
-                <ul class="dropdown-menu">
-                  <!-- User image -->
-                  <li class="user-header">
-                    
-                    <p>
-                      Analisis y Desarrollo de Software
-                      <!--<small>www.youtube.com/jcarlosad7</small>-->
-                    </p>
-                  </li>
-                  
-                  <!-- Menu Footer-->
-                  <li class="user-footer">
-                    
-                    <div class="pull-right">
-                      <a href="#" class="btn btn-default btn-flat">Cerrar</a>
-                    </div>
-                  </li>
-                </ul>
-              </li>
-              
-            </ul>
-          </div>
-
-        </nav>
-      </header>
-      <!-- Left side column. contains the logo and sidebar -->
-      <aside class="main-sidebar">
-        <!-- sidebar: style can be found in sidebar.less -->
-        <section class="sidebar">
-          <!-- Sidebar user panel -->
-                    
-          <!-- sidebar menu: : style can be found in sidebar.less -->
-          <ul class="sidebar-menu">
-            <li class="header"></li>
-
-             <li class="treeview">
-              <a href="#">
-                <i class="fa fa-folder"></i> <span>Categoría</span>
-                <i class="fa fa-angle-left pull-right"></i>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="configuracion/usuario"><i class="fa fa-circle-o"></i> Categorias</a></li>
-                
-              </ul>
-            </li>
-            
-            <li class="treeview">
-              <a href="#">
-                <i class="fa fa-laptop"></i>
-                <span>Ficha Tecnica</span>
-                <i class="fa fa-angle-left pull-right"></i>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="almacen/categoria"><i class="fa fa-circle-o"></i>Estudiante</a></li>
-                <li><a href="almacen/categoria"><i class="fa fa-circle-o"></i>Rendimiento</a></li>
-              </ul>
-            </li>
-            
-            <li class="treeview">
-              <a href="#">
-                <i class="fa fa-th"></i>
-                <span>Calendario</span>
-                 <i class="fa fa-angle-left pull-right"></i>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="compras/ingreso"><i class="fa fa-circle-o"></i>Eventos</a></li>
-                
-              </ul>
-            </li>
-            <li class="treeview">
-              <a href="#">
-                <i class="fa fa-shopping-cart"></i>
-                <span>Pagos</span>
-                 <i class="fa fa-angle-left pull-right"></i>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="ventas/venta"><i class="fa fa-circle-o"></i>Mensualidad</a></li>
-               <!-- <li><a href="ventas/cliente"><i class="fa fa-circle-o"></i>Contadores</a></li>-->>
-              </ul>
-            </li>
-                       
-            <li class="treeview">
-              <a href="#">
-                <i class="fa fa-folder"></i> <span>Acceso</span>
-                <i class="fa fa-angle-left pull-right"></i>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="configuracion/usuario"><i class="fa fa-circle-o"></i> Usuarios</a></li>
-                
-              </ul>
-            </li>
-             <li>
-              <a href="#">
-                <i class="fa fa-plus-square"></i> <span>Ayuda</span>
-                <small class="label pull-right bg-red">PDF</small>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i class="fa fa-info-circle"></i> <span>Acerca De...</span>
-                <small class="label pull-right bg-yellow">IT</small>
-              </a>
-            </li>
-                        
-          </ul>
-        </section>
-        <!-- /.sidebar -->
-      </aside>
-
-
-
-
+      <?php
+    require_once("../../partials/header.php");    
+        require_once("../../partials/menu.php");
+      ?>
 
        <!--Contenido-->
       <!-- Content Wrapper. Contains page content -->
@@ -217,24 +99,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
           <div class="row">
             <div class="col-md-12">
               <div class="box">
-                <div class="box-header with-border">
-                  <h3 class="box-title">Escuela de Futbol Coaching F.C</h3>
-                  <div class="box-tools pull-right">
-                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-                    
-                    <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-                  </div>
-                </div>
+                <?php
+                  require_once("../../partials/box_header_grilla.php");
+                ?>
                 <!-- /.box-header -->
                 <div class="box-body">
                     <div class="row">
                       <div class="col-md-12">
-
-                              <!--Contenido OJO AQUI INICIA MI VISTA-->
-     
-                                <div class="row">
+                          <div class="row">
                                   <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                  <h3>Nueva Categoría</h3>
+                                  <h3>Nueva evento</h3>
                                     <?php 
                                    if ($errores)
                                   { 
@@ -250,55 +124,80 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
                                   ?>
                                   </div>
                               </div>
-                                                                 
-                                                                 
-                                  <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-										<div class="form-group">
-											<label for="nombre">Nombre</label>
-											<input type="text" name="nombre" class="form-control" required value="<?php if(isset($nombre)) echo $nombre?>" placeholder="Nombre...">        
-										</div>
-										<div class="form-group">
-										  <label for="descripcion">Descripción</label>
-										  <input type="text" name="descripcion" class="form-control" required value="<?php if(isset($descripcion)) echo $descripcion?>" placeholder="Descripción...">      
-										</div>
-										<div class="form-group">
-											<button class="btn btn-primary" type="submit">Guardar</button>
-											<button class="btn btn-danger" type="reset">Cancelar</button>
-										</div>
-								  </form>
-
-                                  </div>
-                                </div>
-                              <!--Fin Contenido OJO AQUI TERMINA MI VISTA-->
-                           </div>
-                        </div>
-                        
-                      </div>
-                    </div><!-- /.row -->
-                </div><!-- /.box-body -->
-              </div><!-- /.box -->
-            </div><!-- /.col -->
-          </div><!-- /.row -->
-
-        </section><!-- /.content -->
-      </div><!-- /.content-wrapper -->
-      <!--Fin-Contenido-->
-      <footer class="main-footer">
-        <div class="pull-right hidden-xs">
-          <b>Version</b> 1.2.0
+      <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <div>
+          <input name="id_calendario" id="identificador" type="numb" class="form-control" placeholder="Numero de calendario" value="<?php if(isset($id_calendario)) echo $id_calendario?>"> 
+          <span class="help-block"></span>
         </div>
-        <strong>Copyright &copy; 2019-2020   <!--<a href="www.incanatoit.com">IncanatoIT</a>.</strong> All rights reserved.-->
-      </footer>
-
-      
-    <!-- jQuery 2.1.4 -->
-    <script src="../js/jQuery-2.1.4.min.js"></script>
-    <!-- Bootstrap 3.3.5 -->
-    <script src="../js/bootstrap.min.js"></script>
-    <script src="../js/bootstrap-select.min.js"></script>
-
-    <!-- AdminLTE App -->
-    <script src="../js/app.min.js"></script>
-    
+        <div>
+          <input name="nombre_calendario" id="nombre" type="text" class="form-control" placeholder="nombre de calendario" value="<?php if(isset($nombre_calendario)) echo $nombre_calendario?>"> 
+          <span class="help-block"></span>
+        </div>
+        <div>
+          <input name="estado_calendario" id="estado" type="text" class="form-control" placeholder="estado del calendario" value="<?php if(isset($estado_calendario)) echo $estado_calendario?>"> 
+          <span class="help-block"></span>
+        </div>
+        <div>
+          <input name="fecha_calendario" id="fecha" type="date" class="form-control" placeholder="fecha del calendario" value="<?php if(isset($fecha_calendario)) echo $fecha_calendario?>"> 
+          <span class="help-block"></span>
+        </div>
+        <div>
+          <input name="descripcion_calendario" id="descripcion" type="text" class="form-control" placeholder="descripcion del calendario" value="<?php if(isset($descripcion_calendario)) echo $descripcion_calendario?>"> 
+          <span class="help-block"></span>
+        </div>
+        <div >
+                  
+                   
+        <select name="funcionario" class="form-control" data-live-search="true">
+        <option disabled selected>Seleccione un funcionario</option>
+        <?php
+        require_once("../../../modelo/modelo_funcionario.php");
+        $fun = new funcionario();
+        if ($resultado=$fun->buscar('funcionario',null))
+        {
+        foreach ($resultado as $valor)
+                          {
+        ?>
+        <option value="<?php echo $valor['id_funcionario'];?>"><?php echo $valor['nombres'];?> <?php echo $valor['apellidos'];?></option>
+        <?php                          
+           }
+        }
+            ?>
+        </select>
+                    <span class="help-block"></span> 
+                
+        </div>
+        
+        <button class="btn btn-block bt-login" type="submit" id="submit_btn" data-loading-text="Agregar....">Agregar usuario</button>
+                <?php 
+                 if ($errores)
+                { 
+                ?>
+                       <ul style="color: #f00;">
+                <?php 
+                       foreach ($errores as $error):?>
+                         <li> <?php echo $error ?> </li>
+                <?php  endforeach; ?>
+                       </ul>
+                <?php 
+                }
+                ?>
+        
+      </form>
+      <div>
+        
+      </div>
+        </div>   
+    </div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+             
+              
+       </section>
+        </div>
+      </div>
   </body>
 </html>
+
