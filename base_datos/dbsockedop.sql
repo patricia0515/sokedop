6673ff01a4d657d5a04653863c59c686961b7336
patricia0515 <mprivera12@misena.edu.co>
2020-12-04 12:58:14 -0500

    Base de datos actualizado

--- base_datos/dbsockedop.sql
+++ base_datos/dbsockedop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-10-2020 a las 04:11:23
+-- Tiempo de generación: 04-12-2020 a las 18:47:09
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.2.33
 
@@ -31,10 +31,17 @@ CREATE TABLE `calendario` (
   `id_calendario` int(4) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `estado` varchar(20) NOT NULL,
-  `fecha` datetime NOT NULL,
+  `fecha` date NOT NULL,
   `descripcion` varchar(300) NOT NULL,
   `funcionario` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `calendario`
+--
+
+INSERT INTO `calendario` (`id_calendario`, `nombre`, `estado`, `fecha`, `descripcion`, `funcionario`) VALUES
+(1, 'Entrenamiento', 'Activo', '2021-01-20', 'Entrenamiento con Juvenil', 2);
 
 -- --------------------------------------------------------
 
@@ -54,9 +61,11 @@ CREATE TABLE `categoria` (
 --
 
 INSERT INTO `categoria` (`id_categoria`, `nombre`, `descripcion`, `condicion`) VALUES
-(1, 'Benjamines', 'Niños de 7-10 Años', 1),
-(2, 'Pollos', 'sdmvnasflvafnb', 1),
-(3, 'CHIKIS', 'de poquitos a muchos', 1);
+(1, 'Benjamines', 'Niños de 7-10 Años', 0),
+(4, 'Nueva Jimmy', 'Prueba de Función y de Edición', 1),
+(7, 'Chikis', 'Descripción breve de la categoría', 1),
+(8, 'Juvenil', 'Esta es una prueba de adición de registro en el módulo Categoría.', 1),
+(9, 'Prejuvenil', 'Prueba de Condición', 0);
 
 -- --------------------------------------------------------
 
@@ -89,8 +98,9 @@ CREATE TABLE `estudiante` (
 --
 
 INSERT INTO `estudiante` (`no_documento`, `tipo_documento`, `nombres`, `apellidos`, `estado`, `direccion`, `barrio`, `celular`, `email`, `foto`, `nombre_acudiente`, `apellidos_acudiente`, `tel_acudiente`, `email_acudiente`, `parentesco_acu`, `funcionario`, `categoria`) VALUES
-(11111, 'R.C', '1111', '1111', 'Activo', '1111', '1111', '1111', '11111@hotmail.com', 'DiagramaClases.jpeg', '222222222', 'dcsdcdcasdcasdc', '1234234531234', 'fvdfvdfvdfvf@vwfevwefvwef', 'vefvwefvef', 3, 3),
-(1165489887, 'C.C', 'Juan Gillermo', 'Cuadrado', 'Activo', 'Trv 34 87-45 Sur', 'Chuniza', '3214567834', 'juancuadrado@gmail.c', 'cuadrado.jpg', 'Maria', 'Castillo', '7777270', 'catillo1245@gmail.com', 'Tia', 1, 1);
+(1024550460, 'T.I', 'Alan Felipe', 'Rivera Vallejos', 'Activo', 'Kra 45 67 43 ', 'Olarte', '3219828370', 'alan_vallejos@hotmai', 'bacca.jpg', 'Martha Patricia', 'Mejia Lopez', '7777777', 'mejia@hotmail.com', 'tia', 3, 8),
+(1165489887, 'C.C', 'Juan Gillermo', 'Cuadrado', 'Activo', 'Trv 34 87-45 Sur', 'Chuniza', '3214567834', 'juancuadrado@gmail.c', 'cuadrado.jpg', 'Maria', 'Castillo', '7777270', 'catillo1245@gmail.com', 'Tia', 1, 1),
+(2147483647, 'T.I', 'fv vdfvv', 'asdvasdv', 'Activo', 'asdvdsva', 'asdvasdvasc', '2346546454', 'fdvdfvadfvd@vsdvfdvf', 'logo.jpg', 'afvasdvasfvas', 'sdvasdvsv', 'vasdvasdvasdv', 'asdvasvasdv@gsfdbvdfv', 'dvasvasfva', 3, 9);
 
 -- --------------------------------------------------------
 
@@ -135,9 +145,15 @@ CREATE TABLE `funcionario` (
 --
 
 INSERT INTO `funcionario` (`id_funcionario`, `usuario`, `tipo_documento`, `nombres`, `apellidos`, `celular`, `direccion`, `estado`) VALUES
-(1, '1000366300', 'C.C', 'Angie', 'Castillo', '3219334849', 'tv 97 b sur # 45-45', 'activo'),
-(2, '1000366269', 'C.C', 'Jimmy', 'Benavides', '3219337719', 'tv 5 b este # 45-32', 'activo'),
-(3, '465464363', 'C.C', 'dfhdfhgdf', 'sdfgsdgns', '324632546546', 'SD vS df arg hbse5s62546er g', 'Activo');
+(1, '1000366300', 'C.C', 'Angie', 'Castillo', '3219334849', 'tv 97 b sur # 45-45', 'Activo'),
+(2, '1000366269', 'C.C', 'Jimmy', 'Benavides', '3219337719', 'tv 5 b este # 45-32', 'Activo'),
+(3, '465464363', 'C.C', 'JUANITO ANTONIO', 'PEREZ CASTRO', '324632546546', 'Kra 9 123 98 34 ap 10-01', 'Activo'),
+(4, '1111111', 'C.C', 'Juan Alejandro', 'Parra Gonzalez', '3111111111', 'call1 1 11-11', 'Activo'),
+(10, '345245245345', 'C.C', 'AMANDA TERESA', 'MENDEZ LOPEZ', '345245', 'sfdvsfvfvdfv', 'Activo'),
+(11, '222222222', 'C.C', 'PRUEBA ', 'NUMERO 2', '3112233445', 'CALLE 1 3 4', 'Activo'),
+(12, '4565645645', 'C.C', 'PEDRO NEL', 'DUARTE QUIROZ', '3208374593', 'KRA 56 75 89', 'Activo'),
+(13, '333333333', 'C.C', 'AMADA ROSA', 'PEREZ', '3112456789', 'CALL 45 87 34', 'Activo'),
+(14, '55555555555', 'C.C', 'Ana Maria', 'Veloza Sarmiento', '3555555555', 'Kra 55 55 55', 'Activo');
 
 -- --------------------------------------------------------
 
@@ -159,8 +175,7 @@ CREATE TABLE `mensualidad` (
 --
 
 INSERT INTO `mensualidad` (`id_mensualidad`, `valor`, `fecha_pago`, `mes`, `estudiante`, `funcionario`) VALUES
-(1, '43000.00', '2020-06-07', 'junio', 1165489887, 1),
-(2, '44000.00', '2020-05-07', 'mayo', 1165489887, 1);
+(1, '43000.00', '2020-06-07', 'junio', 1165489887, 1);
 
 -- --------------------------------------------------------
 
@@ -196,7 +211,13 @@ INSERT INTO `usuario` (`doc_usuario`, `clave_usuario`, `mail_usuario`, `tipo_usu
 ('1000366269', '12546', 'jimmybrandon03@gmail.com', 'Administrador'),
 ('1000366300', '12345', 'angiecastillo11@gmail.com', 'Administrador'),
 ('1024463821', '1024', 'patriciariveravallejso@outlook.com', 'Admi'),
-('465464363', '12345', 'mvlsdfnb@hotmail.com', 'Entrenador');
+('1111111', '1111', 'juanparra@homail.com', 'Entrenador'),
+('222222222', '2222', 'prueba@hotmail.com', 'Contador'),
+('333333333', '3333', 'adamaris@hotmail.com', 'Contador'),
+('345245245345', 'casdsadcawd', 'fsvfvfsvefv@advasfvfv', 'Contador'),
+('4565645645', '5645', 'pedro@gmail.com', 'Contador'),
+('465464363', '12345', 'mvlsdfnb@hotmail.com', 'Entrenador'),
+('55555555555', '5555', 'anamaria@gmail.com', 'Contador');
 
 --
 -- Índices para tablas volcadas
@@ -266,13 +287,13 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `calendario`
 --
 ALTER TABLE `calendario`
-  MODIFY `id_calendario` int(4) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_calendario` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id_categoria` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_categoria` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `ficha_tecnica`
@@ -284,7 +305,7 @@ ALTER TABLE `ficha_tecnica`
 -- AUTO_INCREMENT de la tabla `funcionario`
 --
 ALTER TABLE `funcionario`
-  MODIFY `id_funcionario` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_funcionario` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `mensualidad`
@@ -344,20 +365,3 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-select 
-		m.id_mensualidad as mensualidad,
-		m.valor as valor, 
-		m.fecha_pago as fecha , 
-		m.mes as mes, 
-        e.nombres as nombre,
-        e.apellidos as apellido, 
-        f.nombres as nombres,
-        f.apellidos as apellidos
-		from mensualidad as m
-		inner join estudiante as e
-        on m.estudiante = e.no_documento
-        inner join mensualidad as m
-        on m.funcionario = f.no_documento
-
-		order by ID desc
